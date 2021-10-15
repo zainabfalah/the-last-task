701fa6421a20aaafdd6dd2cd7d8d6c2000884285
Bilal Raad <bilal52040@gmail.com>
2021-10-15 22:37:10 +0300

    added some comments

--- lib/API/post_api.dart
+++ lib/API/post_api.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/models/post.dart';
 
+///this represents the data that we get from the internet
 List posts = [
   {
     'title': 'hello',
@@ -21,6 +22,9 @@ List posts = [
 
 class PostApi {
   Future<List<Post>> getPosts() async {
+    //Creates a future that runs its computation after a delay.
+    // The computation will be executed after the given duration has passed
+    //(here dart will continue after one second)
     await Future.delayed(Duration(seconds: 1));
 
     List<Post> userPosts = [];
